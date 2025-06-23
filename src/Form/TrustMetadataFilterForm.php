04abbddbd739f4d0874a856f85c473e5cef0df97
Joshua Nicholson <94021017+jnicholCU@users.noreply.github.com>
2025-06-23 10:21:19 -0600

    Filter, topics, and cron update
    Updated filters
    Update trust topics
    Add cron to grab previously made pages if they don't propagate on install.

---
+++ src/Form/TrustMetadataFilterForm.php
@@ -0,0 +1,107 @@
+<?php
+
+namespace Drupal\ucb_trust_schema\Form;
+
+use Drupal\Core\Form\FormBase;
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\taxonomy\Entity\Term;
+
+class TrustMetadataFilterForm extends FormBase {
+  /**
+   * {@inheritdoc}
+   */
+  public function getFormId() {
+    return 'trust_metadata_filter_form';
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildForm(array $form, FormStateInterface $form_state) {
+    $request = \Drupal::request();
+    $query = $request->query->all();
+
+    $form['trust_role'] = [
+      '#type' => 'select',
+      '#title' => $this->t('Trust Role'),
+      '#options' => [
+        '' => $this->t('- Any -'),
+        'primary_source' => $this->t('Primary Source'),
+        'secondary_source' => $this->t('Secondary Source'),
+        'subject_matter_contributor' => $this->t('Subject Matter Contributor/Expert'),
+        'unverified' => $this->t('Unverified'),
+      ],
+      '#default_value' => $query['trust_role'] ?? '',
+    ];
+
+    $form['trust_scope'] = [
+      '#type' => 'select',
+      '#title' => $this->t('Trust Scope'),
+      '#options' => [
+        '' => $this->t('- Any -'),
+        'department_level' => $this->t('Department-level'),
+        'college_level' => $this->t('College-level'),
+        'administrative_unit' => $this->t('Administrative-unit'),
+        'campus_wide' => $this->t('Campus-wide'),
+      ],
+      '#default_value' => $query['trust_scope'] ?? '',
+    ];
+
+    // Get all trust topics.
+    $topic_options = ['' => $this->t('- Any -')];
+    $terms = \Drupal::entityTypeManager()->getStorage('taxonomy_term')->loadTree('trust_topics');
+    foreach ($terms as $term) {
+      $topic_options[$term->tid] = $term->name;
+    }
+    $form['trust_topics'] = [
+      '#type' => 'select',
+      '#title' => $this->t('Trust Topic'),
+      '#options' => $topic_options,
+      '#default_value' => $query['trust_topics'] ?? '',
+    ];
+
+    $form['trust_syndication_enabled'] = [
+      '#type' => 'select',
+      '#title' => $this->t('Syndication Enabled'),
+      '#options' => [
+        '' => $this->t('- Any -'),
+        '1' => $this->t('Yes'),
+        '0' => $this->t('No'),
+      ],
+      '#default_value' => $query['trust_syndication_enabled'] ?? '',
+    ];
+
+    $form['actions'] = [
+      '#type' => 'actions',
+    ];
+    $form['actions']['submit'] = [
+      '#type' => 'submit',
+      '#value' => $this->t('Filter'),
+    ];
+
+    // Use GET method for filtering.
+    $form['#method'] = 'get';
+
+    return $form;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+    // Redirect to the same page with query parameters for filtering.
+    $params = [];
+    foreach ([
+      'trust_role',
+      'trust_scope',
+      'trust_topics',
+      'trust_syndication_enabled',
+    ] as $key) {
+      $value = $form_state->getValue($key);
+      if ($value !== '' && $value !== NULL) {
+        $params[$key] = $value;
+      }
+    }
+    $form_state->setRedirect('<current>', [], ['query' => $params]);
+  }
+} 
--- src/TrustMetadataListBuilder.php
+++ src/TrustMetadataListBuilder.php
@@ -5,6 +5,8 @@ namespace Drupal\ucb_trust_schema;
 use Drupal\Core\Entity\EntityInterface;
 use Drupal\Core\Entity\EntityListBuilder;
 use Drupal\Core\Link;
+use Drupal\ucb_trust_schema\Form\TrustMetadataFilterForm;
+use Drupal\Core\Form\FormBuilderInterface;
 
 /**
  * Defines a class to build a listing of Trust Metadata entities.
@@ -19,6 +21,7 @@ class TrustMetadataListBuilder extends EntityListBuilder {
     $header['node'] = $this->t('Node');
     $header['trust_role'] = $this->t('Trust Role');
     $header['trust_scope'] = $this->t('Trust Scope');
+    $header['trust_topics'] = $this->t('Trust Topics');
     $header['trust_syndication_enabled'] = $this->t('Syndication Enabled');
     return $header + parent::buildHeader();
   }
@@ -32,8 +35,56 @@ class TrustMetadataListBuilder extends EntityListBuilder {
     $row['node'] = $entity->get('node_id')->entity ? $entity->get('node_id')->entity->toLink() : '';
     $row['trust_role'] = $entity->get('trust_role')->value;
     $row['trust_scope'] = $entity->get('trust_scope')->value;
+    $topics = [];
+    foreach ($entity->get('trust_topics') as $topic) {
+      if ($topic->entity) {
+        $topics[] = $topic->entity->label();
+      }
+    }
+    $row['trust_topics'] = implode(', ', $topics);
     $row['trust_syndication_enabled'] = $entity->get('trust_syndication_enabled')->value ? $this->t('Yes') : $this->t('No');
     return $row + parent::buildRow($entity);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function render() {
+    $form = \Drupal::formBuilder()->getForm(TrustMetadataFilterForm::class);
+    $build = [];
+    $build['filter_form'] = $form;
+    $build += parent::render();
+    return $build;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getEntityIds() {
+    $query = $this->getStorage()->getQuery();
+    $query->accessCheck(FALSE);
+    $request = \Drupal::request();
+    $params = $request->query->all();
+
+    if (!empty($params['trust_role'])) {
+      $query->condition('trust_role', $params['trust_role']);
+    }
+    if (!empty($params['trust_scope'])) {
+      $query->condition('trust_scope', $params['trust_scope']);
+    }
+    if (!empty($params['trust_topics'])) {
+      $query->condition('trust_topics', $params['trust_topics']);
+    }
+    if (array_key_exists('trust_syndication_enabled', $params) && $params['trust_syndication_enabled'] !== '' && isset($params['trust_syndication_enabled'])) {
+      $query->condition('trust_syndication_enabled', (bool) $params['trust_syndication_enabled']);
+    }
+
+    // Add table sort and pager.
+    $header = $this->buildHeader();
+    $query->tableSort($header);
+    $query->pager($this->limit);
+
+    return $query->execute();
   }
 
 } 
